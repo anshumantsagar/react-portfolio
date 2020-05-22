21b2d7e8fd1ce19257ba907de5d984cd5399d731
anshumantsagar <anshumantsagar@gmail.com>
2020-05-23 00:41:07 +0530

    About Page complete

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,11 +1,26 @@
 import React from 'react';
-
+import Hero from '../components/Hero';
+import Content from '../components/Content';
 
 function AboutPage(props) {
 
     return(
         <div>
-            AboutPage Works!
+            <Hero title={props.title} />
+
+            <Content>
+                <p>Hello, my name is Anshumant. I'm a Angular and React developer with experience in Angular.</p>
+
+                <p>Currently looking for the oportunities to put my skills in the right work and learn new things.</p>
+
+                <p>My dream is to start my own business one day and become an entrepreneur (I have some ideas).</p>
+
+                <p>I'm constantly learning new things. Currently gaining more experience with React</p>
+
+                <p>You can check out my GitHub account <a href="https://github.com/anshumantsagar" target="_blank" rel="noopener noreferrer">here</a>.</p>
+
+                <p>You can check out my Linkdin account <a href="https://www.linkedin.com/in/anshumantsagar/" target="_blank" rel="noopener noreferrer">here</a>.</p>
+            </Content>
         </div>
     );
 
