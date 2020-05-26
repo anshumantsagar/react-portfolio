9334dd05d99e870467de08fb86a0cae4505c57e7
anshumantsagar <anshumantsagar@gmail.com>
2020-05-26 19:01:41 +0530

    adding dependencies

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -41,7 +41,7 @@ class ContactPage extends React.Component {
             disabled: true
         });
 
-        Axios.post('http://localhost:3001/api/email', this.state)
+        Axios.post('http://localhost:3030/api/email', this.state)
             .then(res => {
                 if(res.data.success) {
                     this.setState({
